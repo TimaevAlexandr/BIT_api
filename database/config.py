163e83d697e223e020f611b85b776bf98209b2a8
TimaevAlexandr <timaevas2005@mail.ru>
2023-09-01 16:32:10 +0300

    Delete fake_database from config.py

--- database/config.py
+++ database/config.py
@@ -10,70 +10,9 @@ username = "admin"
 word = "admin"
 # for that we import (1 line)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# make hashed password
 password = pwd_context.hash(word)
-
-
 SECRET_KEY = "SUPER_SECRET_KEY"
 ALGORITHM = "HS256"
-fake_database = {'users': [
-    {
-        "id": 1,
-        "name": "Anna",
-        "nick": "Anny42",
-        "balance": 15300
-    },
-
-    {
-        "id": 2,
-        "name": "Dima",
-        "nick": "dimon2319",
-        "balance": 160.23
-    },
-    {
-        "id": 3,
-        "name": "Vladimir",
-        "nick": "Vova777",
-        "balance": 200.1
-    },
-    {
-        "id": 4,
-        "name": "Anna1",
-        "nick": "Anny420",
-        "balance": 15300
-    },
-
-    {
-        "id": 5,
-        "name": "Dima15",
-        "nick": "dimon2342",
-        "balance": 160.23
-    },
-    {
-        "id": 6,
-        "name": "Vladimir666",
-        "nick": "Vova13",
-        "balance": 200.1
-    },
-    {
-        "id": 7,
-        "name": "Anna2",
-        "nick": "Anny421",
-        "balance": 15300
-    },
-
-    {
-        "id": 8,
-        "name": "Dima23",
-        "nick": "dimon19",
-        "balance": 160.23
-    },
-    {
-        "id": 9,
-        "name": "Vladimir0nion",
-        "nick": "Vova123",
-        "balance": 200.1
-    },
-
-], }
 
 api_url = "http://127.0.0.1:8000"
